e935b0a1b5418548fdfd752b7040101a1faf5351
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-06 23:22:30 -0400

    Rearrange imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const client = new QueryClient();
 
