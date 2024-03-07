963330b33e7f82db35d914a834ef294756ecdcc9
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-07 10:55:37 -0400

    Add pagination

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -8,16 +8,24 @@ interface Post {
     userId: number;
 }
 
-const usePosts = (userId: number | undefined) => {
-    const fetchPosts = () => axios
-        .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
-            params: { userId }
-        })
-        .then(res => res.data)
+interface PostQuery {
+    page: number,
+    pageSize: number
+}
 
+const usePosts = (query: PostQuery) => {
     return useQuery<Post[], Error>({
-        queryKey: userId ? ['users', userId, 'posts'] : ['posts'],
-        queryFn: fetchPosts
+        queryKey: ['posts', query],
+        queryFn: () => axios
+            .get<Post[]>('https://jsonplaceholder.typicode.com/posts', {
+                params: {
+                    _start: (query.page - 1) * query.pageSize,
+                    _limit: query.pageSize
+                }
+            })
+            .then(res => res.data),
+        staleTime: 1 * 60 * 1000, // 1 minute
+        keepPreviousData: true
     })
 }
 
