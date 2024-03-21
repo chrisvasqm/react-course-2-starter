fe4342c61227b8e3b62ea75c975dac8d5b9901d8
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-21 09:26:31 -0400

    Add route with parameter

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,11 +3,13 @@ import HomePage from "./HomePage";
 import UserListPage from "./UserListPage";
 import ErrorPage from "./ErrorPage";
 import ContactPage from "./ContactPage";
+import UserDetailPage from "./UserDetailPage";
 
 const router = createBrowserRouter([
     { path: '/', element: <HomePage /> },
     { path: '/*', element: <ErrorPage /> },
     { path: '/users', element: <UserListPage /> },
+    { path: '/users/:id', element: <UserDetailPage />},
     { path: '/contact', element: <ContactPage /> },
 ])
 
