85ae36beb423bc9504ac6c9dea1f041bfd0dcc31
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-24 09:44:26 -0400

    Add Users Navbar

--- src/routing/UsersPage.tsx
+++ src/routing/UsersPage.tsx
@@ -1,20 +1,24 @@
 import { Navigate, Outlet } from "react-router-dom";
 import UserList from "./UserList";
 import useAuth from "./hooks/useAuth";
+import NavBar from "./NavBar";
 
 function UsersPage() {
     const { user } = useAuth();
     if (!user) return <Navigate to={'/login'} />
 
     return (
-        <div className="row">
-            <div className="col">
-                <UserList />
+        <>
+            <NavBar />
+            <div className="row">
+                <div className="col">
+                    <UserList />
+                </div>
+                <div className="col">
+                    <Outlet />
+                </div>
             </div>
-            <div className="col">
-                <Outlet />
-            </div>
-        </div>
+        </>
     )
 }
 
